b1b6ea38719b96075b1287dfa3f0f12af3f6bd69
gtxx9903 <gtxripmxtlol@gmail.com>
2025-07-08 20:22:58 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,28 @@
 using namespace geode::prelude;
 
 bool confirmRestart = Mod::get()->getSavedValue<bool>("confirm-restart", false);
+
 #include <Geode/modify/PauseLayer.hpp>
 class $modify(MyPauseLayer, PauseLayer) {
     void customSetup() {
         PauseLayer::customSetup();
 
         auto checkbox = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(MyPauseLayer::onHello), 1.0f);
-		checkbox->toggle(confirmRestart);
-		checkbox->setID("confirm-restart-checkbox"_spr);
+	checkbox->toggle(confirmRestart);
+	checkbox->setID("confirm-restart-checkbox"_spr);
 
-		auto confirmRestartLabel = CCLabelBMFont::create("Confirm Restart", "bigFont.fnt");
-		confirmRestartLabel->setID("confirm-restart-label"_spr);
+	auto confirmRestartLabel = CCLabelBMFont::create("Confirm Restart", "bigFont.fnt");
+	confirmRestartLabel->setID("confirm-restart-label"_spr);
 
-		auto menu = this->getChildByID("bottom-button-menu");
-		menu->setScale(0.6f);
-		menu->addChild(checkbox);
-		menu->addChild(confirmRestartLabel);
-		menu->updateLayout();
+	auto menu = this->getChildByID("bottom-button-menu");
+	menu->setScale(0.6f);
+	menu->addChild(checkbox);
+	menu->addChild(confirmRestartLabel);
+	menu->updateLayout();
     }
     void onHello(CCObject * sender) {
-		confirmRestart = !confirmRestart;
-		Mod::get()->setSavedValue("confirm-restart", confirmRestart);
+	confirmRestart = !confirmRestart;
+	Mod::get()->setSavedValue("confirm-restart", confirmRestart);
     }
     void onRestart(CCObject * sender) {
         if (confirmRestart) {
@@ -36,12 +37,12 @@ class $modify(MyPauseLayer, PauseLayer) {
 
                         playLayer->resetLevel();
                         this->onResume(nullptr);
-					}
+		    }
                 }
             );
         }
         else {
-			PauseLayer::onRestart(sender);
+	    PauseLayer::onRestart(sender);
         }
     }
 };
